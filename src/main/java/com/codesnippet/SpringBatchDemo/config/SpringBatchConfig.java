36386a9429a7cef794841a860c0904b27980f478
magesh0407 <magesh0407@gmail.com>
2026-03-13 18:14:09 +0530

    Revert "remove unwanted code"
    This reverts commit 389e04b88247d2f6709dcc4042255be859734a16.

--- src/main/java/com/codesnippet/SpringBatchDemo/config/SpringBatchConfig.java
+++ src/main/java/com/codesnippet/SpringBatchDemo/config/SpringBatchConfig.java
@@ -1,7 +1,11 @@
 package com.codesnippet.SpringBatchDemo.config;
 
+import com.codesnippet.SpringBatchDemo.entity.Person;
+import com.codesnippet.SpringBatchDemo.repository.PersonRepository;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -16,10 +20,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.transaction.PlatformTransactionManager;
 
-import com.codesnippet.SpringBatchDemo.entity.Person;
-import com.codesnippet.SpringBatchDemo.repository.PersonRepository;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class SpringBatchConfig {
