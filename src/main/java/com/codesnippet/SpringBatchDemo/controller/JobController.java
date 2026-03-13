07eb8d5a61166b6dddc1e2098a1889f2ac0d195a
magesh0407 <magesh0407@gmail.com>
2026-03-13 16:58:59 +0530

    Removed unwanted import

--- src/main/java/com/codesnippet/SpringBatchDemo/controller/JobController.java
+++ src/main/java/com/codesnippet/SpringBatchDemo/controller/JobController.java
@@ -12,7 +12,6 @@ import org.springframework.batch.core.repository.JobExecutionAlreadyRunningExcep
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
